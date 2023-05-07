0b38c8477d863fd69da89aa44e03d9dcdcb6af26
Aarish <aarishm767@gmail.com>
2023-05-07 22:06:04 +0530

    login page created and linked with backend

--- src/components/DataForm.js
+++ src/components/DataForm.js
@@ -1,54 +1,58 @@
 import React, { useState } from 'react'
+import Test from './Test';
 
 export default function DataForm() {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setlastName] = useState('')
     const [email, setEmail] = useState('')
-    const [favcolor, setFavcolor] = useState('')
-    const [favcar, setFavcar] = useState('')
+    const [password, setPassword] = useState('')
+    const [isFetching, setIsFetching] = useState(false)
 
     const submitData=(e)=>{
         e.preventDefault();
-        const user = {firstName, lastName, email, favcolor, favcar};
-        console.log(user);
+        
+        const userCreds = {email, password};
+        console.log(userCreds);
 
-        fetch("http://localhost:8080/users", {
+        let status = null;
+
+        fetch("http://localhost:8080/auth/login", {
             method : "POST",
-            body : JSON.stringify(user),
+            body : JSON.stringify(userCreds),
             headers : {"Content-Type" : "application/json"}
+        }).then(response => {
+            let res = response.text();
+
+            status = response.status;
+            console.log(res);
+            console.log(status);
+        }).then(response => {
+            if(status === 201)
+                setIsFetching(true)
         });
     }
 
 
   return (
     <>
-        <div className='container mt-3'>
-            <form>
-                <div className="row mb-3 w-50 mx-auto">
-                    <label className="col form-label">First Name</label>
-                    <input type="text" className="col form-control" id="firstname" value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
-                </div>
-                <div className="row mb-3 w-50 mx-auto">
-                    <label className="col form-label">Last Name</label>
-                    <input type="text" className="col form-control" id="lastname" value={lastName} onChange={(e)=>setlastName(e.target.value)}/>
-                </div>
+        {
+            !isFetching ? 
+            <div className='container mt-3'>
+            <form id='login-form'>
                 <div className="row mb-3 w-50 mx-auto">
                     <label className="col form-label">Email address</label>
                     <input type="email" className="col form-control" id="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <div className="row mb-3 w-50 mx-auto">
-                    <label className="col form-label">Favourite Color</label>
-                    <input type="text" className="col form-control" id="favcolor" value={favcolor} onChange={(e)=>setFavcolor(e.target.value)}/>
-                </div>
-                <div className="row mb-3 w-50 mx-auto">
-                    <label className="col form-label">Favourite Car</label>
-                    <input type="text" className="col form-control" id="favcar" value={favcar} onChange={(e)=>setFavcar(e.target.value)}/>
+                    <label className="col form-label">Password</label>
+                    <input type="password" className="col form-control" id="favcolor" value={password} onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
                 <div className='container text-center mt-4'>
                     <button type="submit" className="text-center btn btn-primary" onClick={submitData}>Submit</button>
                 </div>
             </form>
         </div>
+        : 
+        <Test/>
+        }
     </>
   )
 }
